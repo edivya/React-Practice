408aaa1e985008ed26a79dd660e220c793a3f809
edivya <enjamuridivya@gmail.com>
2020-03-24 18:59:45 -0700

    songs added calling ApI key

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,27 @@
+import { combineReducers } from "redux";
+
+const songsReducer = () => {
+  return [
+    {
+      title: "No Scrubs",
+      duration: "4:05"
+    },
+    { title: "macarena", duration: "2:30" },
+    { title: "All Star", duration: "3:15" },
+    {
+      title: "I Want it That Way",
+      duration: "1:45"
+    }
+  ];
+};
+
+const selectedSongReducer = (selectSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+  return selectSong;
+};
+export default combineReducers({
+  songs: songsReducer,
+  selectSong: selectedSongReducer
+});
